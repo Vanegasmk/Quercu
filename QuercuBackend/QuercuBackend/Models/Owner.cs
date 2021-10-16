7e683d5b2f4ffdc57cb939d7cb39293fb95b3f0b
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-16 17:49:58 -0600

    Add Models

---
+++ QuercuBackend/QuercuBackend/Models/Owner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuercuBackend.Models
+{
+    public class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string Telephone { get; set; }
+
+        [Column(TypeName = "varchar(255)")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string IdentificationNumber { get; set; }
+
+
+        [Column(TypeName = "varchar(255)")]
+        public string Address { get; set; }
+
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
