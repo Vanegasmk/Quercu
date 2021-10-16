91dd66ae9e04d83066b5866ed7d47c60b452d67a
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-16 17:49:58 -0600

    Add Models

---
+++ QuercuBackend/QuercuBackend/Models/PropertyType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuercuBackend.Models
+{
+    public class PropertyType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(255)")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}
