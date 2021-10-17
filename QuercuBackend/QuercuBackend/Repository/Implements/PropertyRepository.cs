8ab7bdc8b6153b5679d6263a94aa9d8fb6d37bb7
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-16 22:40:15 -0600

    Add Repositories

--- QuercuBackend/QuercuBackend/Repository/Implements/PropertyRepository.cs
+++ QuercuBackend/QuercuBackend/Repository/Implements/PropertyRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using QuercuBackend.Data;
+using QuercuBackend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QuercuBackend.Repository.Implements
 {
-    public class PropertyRepository
+    public class PropertyRepository : GenericRepository<Property>
     {
+        public PropertyRepository(PropertiesContext propertiesContext) : base(propertiesContext)
+        {
+               
+        }
     }
 }
