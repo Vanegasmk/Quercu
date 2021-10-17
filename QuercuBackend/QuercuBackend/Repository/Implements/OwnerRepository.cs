8acb10b117294c52d66c2950db24d00460637f30
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-16 22:40:15 -0600

    Add Repositories

--- QuercuBackend/QuercuBackend/Repository/Implements/OwnerRepository.cs
+++ QuercuBackend/QuercuBackend/Repository/Implements/OwnerRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using QuercuBackend.Data;
+using QuercuBackend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QuercuBackend.Repository.Implements
 {
-    public class OwnerRepository
+    public class OwnerRepository : GenericRepository<Owner>
     {
+        public OwnerRepository(PropertiesContext propertiesContext) : base(propertiesContext)
+        {
+
+        }
     }
 }
--- QuercuBackend/QuercuBackend/Repository/Implements/PropertyTypeRepository.cs
+++ QuercuBackend/QuercuBackend/Repository/Implements/PropertyTypeRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using QuercuBackend.Data;
+using QuercuBackend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QuercuBackend.Repository.Implements
 {
-    public class PropertyTypeRepository
+    public class PropertyTypeRepository : GenericRepository<PropertyType>
     {
+        public PropertyTypeRepository(PropertiesContext propertiesContext) : base(propertiesContext)
+        {
+
+        }
     }
 }
