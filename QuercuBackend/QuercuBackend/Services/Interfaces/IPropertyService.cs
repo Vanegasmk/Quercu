9e4633ad6a18c285e0e11544194b96b8a97e0741
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-17 13:10:16 -0600

    Add services

---
+++ QuercuBackend/QuercuBackend/Services/Interfaces/IPropertyService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuercuBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuercuBackend.Services.Interfaces
+{
+    public interface IPropertyService
+    {
+        Task<ActionResult<IEnumerable<Property>>> GetAll();
+        Task<ActionResult<Property>> GetOwnerById(int id);
+
+        Task<ActionResult<Property>> Update(int id, Property property);
+        Task<ActionResult<Property>> Delete(int id);
+    }
+}
