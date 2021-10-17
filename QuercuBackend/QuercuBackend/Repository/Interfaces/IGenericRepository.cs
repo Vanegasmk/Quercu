b7eaf488897efc8c6482ee7a4096f5461d7c3733
Kevin Andrés Vanegas Medina <vanegasmk@gmail.com>
2021-10-16 19:56:05 -0600

    Add generic interface

---
+++ QuercuBackend/QuercuBackend/Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuercuBackend.Repository.Interfaces
+{
+    interface IGenericRepository<IEntity> where IEntity : class
+    {
+        Task<IEnumerable<IEntity>> GetAll();
+        Task<IEntity> GetById(int id);
+        Task<IEntity> Create();
+        Task<IEntity> Update();
+        Task Delete(int id);
+
+    }
+}
